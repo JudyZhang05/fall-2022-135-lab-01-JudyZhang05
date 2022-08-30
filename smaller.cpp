2d8e75c53ebd20f5ec3585f8b23e241e5940ba05
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2022-08-30 15:19:08 -0400

    Add files via upload

---
+++ smaller.cpp
@@ -0,0 +1,27 @@
+/* 
+ * Author: Judy Zhang
+ * Course: CSCI-135
+ * Instructor: Professor Maryash
+ * Assignment: Task A. Find the smaller of two integers.
+ * Program asks user for two numbers then compares them to print out the lower number.
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+	int first; int second;
+	cout << "Enter the first number: ";
+	cin >> first;
+	cout << "Enter the second number: ";
+	cin >> second;
+
+	cout << endl;
+
+	if (first < second){
+		cout << "The smaller of the two is: " << first << endl;
+	}
+	else{
+		cout << "The smaller of the two is " << second << endl;
+	}
+	return 0;
+}
